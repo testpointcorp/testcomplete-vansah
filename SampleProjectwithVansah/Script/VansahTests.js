a09f3617fcfe7bdf8c97520e83dd8ca14f3457bf
Shubham Mourya <shubham.mourya@testpoint.com.au>
2024-04-13 04:33:33 +1000

    Added function to send API request

---
+++ SampleProjectwithVansah/Script/VansahTests.js
@@ -0,0 +1,23 @@
+﻿function EventControl_OnStopTest() { 
+    Log.Message("result : "+Project.Variables.VansahResult);
+    // Retrieve variables from project variables
+    var testCaseKey = Project.Variables.testCaseKey;
+    var assetKey = Project.Variables.assetKey;
+    var endpoint = Project.Variables.Vansah_URL;
+
+    // Log variable values
+ //   Log.Message("TestCaseKey: " + testCaseKey);
+  //  Log.Message("AssetKey: " + assetKey);
+  //  Log.Message("Endpoint: " + endpoint);
+  
+    // Create request body
+    var requestBody = `{"case": {"key": "${testCaseKey}"}, "asset": {"type": "issue", "key": "${assetKey}"}}`;
+    // Create and send HTTP request
+    var httpRequest = aqHttp.CreateRequest("POST", endpoint);
+    httpRequest.SetHeader("Content-Type", "application/json");
+    httpRequest.SetHeader("Authorization", aqEnvironment.GetEnvironmentVariable("VANSAH_TOKEN"));
+    var httpResponse = httpRequest.Send(requestBody);
+    
+    
+    
+}
